3149b1af0c8f154dc9b9971f00bd9222f7440ca0
DiegoSB9 <diego_sb_95@hotmail.com>
2024-10-23 17:53:20 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@
 let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
 let noun = ['jogger', 'racoon'];
+let ext = ['.com', '.net', '.us', '.io'];
 
 //Bucles "for of" (recorre el array adoptando directamente el valor) para generar la concatenación de las partes para cada dominio
 for (let start of pronoun) { 
   for (let half of adj) {
     for (let end of noun) {
-      console.log("Dominios: "+start+half+end+".com"); 
+      for (let extension of ext )
+      console.log("Dominios: "+start+half+end+extension); 
     }
   }
 }
